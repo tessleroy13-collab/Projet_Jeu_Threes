02a58057da79409d171adf60602966077301efd3
LEROY <tess.leroy@epfedu.fr>
2025-12-15 14:55:38 +0100

    creation projet

---
+++ src/projet_jeu_threes/Projet_Jeu_Threes.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package projet_jeu_threes;
+
+/**
+ *
+ * @author tessl
+ */
+public class Projet_Jeu_Threes {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
